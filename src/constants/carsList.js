60d0aad2253660186e976d77d3e3c7b6e58975e7
Ali chatraei <avida.chatraee2001@yahoo.com>
2022-09-15 18:30:17 +0430

    Add cars to a array of list

---
+++ src/constants/carsList.js
@@ -0,0 +1,34 @@
+export default carsList = [
+  {
+    id: Math.random(),
+    title: "Model 3",
+    subTitle: "Order Online for Touchless Delivery",
+    image: require("../../assets/images/model4.jpg"),
+    orderLink: "https://www.tesla.com/model3/design",
+    existingInventory: "https://www.tesla.com/inventory/new/m3",
+  },
+  {
+    id: Math.random(),
+    title: "Model Y",
+    subTitle: "Order Online for Touchless Delivery",
+    image: require("../../assets/images/model3.jpg"),
+    orderLink: "https://www.tesla.com/modely/design",
+    existingInventory: "https://www.tesla.com/inventory/new/my",
+  },
+  {
+    id: Math.random(),
+    title: "Model S",
+    subTitle: "Order Online for Touchless Delivery",
+    image: require("../../assets/images/model2.jpg"),
+    orderLink: "https://www.tesla.com/models/design",
+    existingInventory: "https://www.tesla.com/inventory/new/ms",
+  },
+  {
+    id: Math.random(),
+    title: "Model S",
+    subTitle: "Order Online for Touchless Delivery",
+    image: require("../../assets/images/model1.jpg"),
+    orderLink: "https://www.tesla.com/modelx/design",
+    existingInventory: "https://www.tesla.com/inventory/new/mx",
+  },
+];
